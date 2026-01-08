55e887bcc805f78a7829a41f8afbeb65905da9c8
chdo24551-wq <chdo24551@g.nihon-u.ac.jp>
2026-01-08 13:24:01 +0900

    Add files via upload
    最新バージョンに更新（2025/01/08）

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ export default function Footer(){
     return (
         <footer className="footer">
             <div className="content has-text-centered">
-                <p>アップルパイにはカスタードクリームが似合う！</p>
                 <p>
-                    <a href = "https://ja.wikipedia.org/wiki/%E3%82%A2%E3%83%83%E3%83%97%E3%83%AB%E3%83%91%E3%82%A4">アップルパイとは？</a>
+                    <a href = "https://www.google.com/search?sca_esv=23dd50ea98785b26&rlz=1C1SLLM_jaJP1137JP1137&udm=2&fbs=AIIjpHzQki16q-8Z7j6aseYi2jA_awT46aeCpCIps-CmKmRFQnye5ZCZ-joo3KzReil4N41YRRLs-86AZZNOykEHhb7AdsuSQ8j2APaNkZKFznqXKgLLD3-KA4RlvmiBqzuc-4XkTtoGNHLvpdjgjrFNZn6u3hsnaC-jHxwng_pwqGegijhpjUR5X2o8SvAtNRkC4Tw2cSB0c90wlk5tj3CYhcAmIzqjI_aVCIOjZu2XHu-ZT1a4PeBKWygqxMZX2xWyCz8GzJSU&q=%E3%82%A2%E3%83%83%E3%83%97%E3%83%AB%E3%83%91%E3%82%A4&sa=X&ved=2ahUKEwik6Pq71KORAxWmsVYBHV-hAJEQtKgLegQIHBAB&biw=1536&bih=738&dpr=1.25">他にも様々なアップルパイが！</a>
                 </p>
+                <a class="top" href = "#top">トップへ戻る</a>
             </div>
         </footer>
     );
