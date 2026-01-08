49b6e7d6048dc710d5a34ce14fbf9de8b62b4fb0
chdo24551-wq <chdo24551@g.nihon-u.ac.jp>
2026-01-08 14:16:02 +0900

    一部の内容の修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,21 +16,3 @@ export default function Header(){
         </header>
     );
 }
-
-export default function Header(){
-    return (
-        <header className="hero is-dark is-bold">
-            <div className="hero-body">
-                <div className="container">
-                    <h1 className="title">アップルパイ愛好倶楽部へようこそ</h1>
-                </div>
-            </div>
-            <nav>
-                <button style={{color:"yellow", backgroundColor:"blue"}} onClick={()=>window.location='#1'}>アップルパイとは？</button>
-                <button style={{color:"yellow", backgroundColor:"blue"}} onClick={()=>window.location='#2'}>アップルパイの歴史</button>
-                <button style={{color:"yellow", backgroundColor:"blue"}} onClick={()=>window.location='#3'}>アップルパイ　ギャラリー?</button>
-                <button style={{color:"yellow", backgroundColor:"blue"}} onClick={()=>window.location='#4'}>アップルパイにおすすめのトッピング</button>
-            </nav>
-        </header>
-    );
-}
